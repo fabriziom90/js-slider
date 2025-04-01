5bb913f0315d78ee01f0d2ffe073ac106d345ea9
fabriziom90 <fabriziomastrobattista@gmail.com>
2025-04-01 12:47:30 +0200

    [UPD] Added autoplay and optimizations

--- js/script.js
+++ js/script.js
@@ -26,6 +26,7 @@ const pics = [
     }
   ];
 
+//   DEFINIZIONE DELLE FUNZIONI
 // definizione della funzione che crea la singola immagine
   const createImage = (galleryImage) => {
     // destrutturiamo il parametro passato nelle sue proprietà
@@ -60,6 +61,43 @@ const renderImages = (array) => {
     gallery.innerHTML = images;
 }
 
+// funzione che mi manda avanti le immagini da vedere
+const nextImage = () => {
+    // rimuovo la classe active dall'elemento attualmente attivo
+    images[activeImage].classList.remove('active');
+    // incremento il valore del cursore
+    activeImage++;
+
+    // se il cursore ha un valore maggiore rispetto alla lunghezza dell'array lo rimetto a zero
+    if(activeImage >= images.length){
+        activeImage = 0;
+    }
+    
+    // imposto la classe active al nuovo elemento
+    images[activeImage].classList.add('active');
+
+    
+}
+
+// funzione che mi manda indietro le immagini da vedere
+const previousImage = () => {
+    // rimuovo la classe active dall'elemento attualmente attivo
+    images[activeImage].classList.remove('active');
+    // decremento il valore del cursore
+    activeImage--;
+
+    // se il cursore ha un valore minore di zero lo imposto all'ultimo elemento dell'array
+    if(activeImage < 0){
+        activeImage = images.length - 1;
+    }
+
+    // imposto la classe active al nuovo elemento
+    images[activeImage].classList.add('active');
+
+}
+
+// CORPO DEL PROGRAMMA
+
 // siamo andati a renderizzare tutte le immagini
 renderImages(pics);
 
@@ -74,33 +112,9 @@ images[activeImage].classList.add('active');
 const nextButton = document.querySelector('.fa-arrow-right');
 const leftButton = document.querySelector('.fa-arrow-left');
 
-nextButton.addEventListener('click', function() {
-    // rimuovo la classe active dall'elemento attualmente attivo
-    images[activeImage].classList.remove('active');
-    // incremento il valore del cursore
-    activeImage++;
+nextButton.addEventListener('click', nextImage);
 
-    // se il cursore ha un valore maggiore rispetto alla lunghezza dell'array lo rimetto a zero
-    if(activeImage >= images.length){
-        activeImage = 0;
-    }
+leftButton.addEventListener('click', previousImage);
 
-    // imposto la classe active al nuovo elemento
-    images[activeImage].classList.add('active');
-
-});
-
-leftButton.addEventListener('click', function(){
-    // rimuovo la classe active dall'elemento attualmente attivo
-    images[activeImage].classList.remove('active');
-    // incremento il valore del cursore
-    activeImage--;
-
-    // se il cursore ha un valore maggiore rispetto alla lunghezza dell'array lo rimetto a zero
-    if(activeImage < 0){
-        activeImage = images.length - 1;
-    }
-
-    // imposto la classe active al nuovo elemento
-    images[activeImage].classList.add('active');
-});
+// autoplay
+const intervalId = setInterval(nextImage, 2000);
